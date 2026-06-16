99e677b2d425cc7e482ad7e072ff27cd38370846
harsh-bitverse <harshhagr3408@gmail.com>
2026-06-16 15:17:42 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-export default function Home(){
+function RotatingCube() {
+  const cubeRef = useRef<any>(null);
+
+  useFrame(() => {
+    if (cubeRef.current) {
+      cubeRef.current.rotation.y += 0.01;
+    }
+  });
+
   return (
-    <main style = {{width : "100vw", height : "100vh"}}>
-      <Canvas camera = {{position : [0, 50, 0]}}>
-        <ambientLight intensity = {1}/>
+    <mesh ref={cubeRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
+}
 
-        <mesh rotation = {[1.5, 0, 0]}>
-          <sphereGeometry args = {[1, 128, 128]}/>
-          <meshStandardMaterial color = "white"/>
-        </mesh>
+export default function Home() {
+  return (
+    <main style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [0, 0, 5] }}>
+        <ambientLight intensity={1} />
+        <RotatingCube />
       </Canvas>
     </main>
   );
